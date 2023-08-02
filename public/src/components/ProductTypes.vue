bd35d6530df673c272ab5674986f39b0d1a2ade7
heyosseus <ratiruxadzee@gmail.com>
2023-08-02 15:13:32 +0400

    refactor: refactor add product components fields to achieve desired clean code

---
+++ public/src/components/ProductTypes.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <div class="w-content bg-gray-950 h-contentH mt-12 rounded-lg drop-shadow-lg">
+      <div class="p-10">
+        <div v-if="selectedProductType === 'DVD'" class="">
+          <label for="size">Size (mb)</label>
+          <Field
+            name="size"
+            type="text"
+            id="size"
+            placeholder="size"
+            class="p-2 rounded-lg border-none outline-none text-black ml-10 w-input"
+            rules="required|number"
+            :value="props.size"
+            @input="$emit('update:size', $event.target.value)"
+          />
+          <ErrorMessage name="size" class="text-sm block text-red-600 mt-4" />
+
+          <p class="text-sm mt-4 text-gray-300">*Please, provide disc space in MB</p>
+        </div>
+        <div v-else-if="selectedProductType === 'Book'">
+          <label for="length">Weight (kg)</label>
+          <Field
+            name="weight"
+            type="text"
+            id="weight"
+            placeholder="weight"
+            class="p-2 rounded-lg border-none outline-none text-black ml-10 w-input"
+            rules="required|number"
+            :value="props.weight"
+            @input="$emit('update:weight', $event.target.value)"
+          />
+          <ErrorMessage name="weight" class="text-sm block text-red-600 mt-4" />
+
+          <p class="text-sm mt-4 text-gray-300">*Please, provide weight in KG</p>
+        </div>
+        <div
+          v-else-if="selectedProductType === 'Furniture'"
+          class="flex flex-col space-y-8 w-content"
+        >
+          <div>
+            <label for="height">Height (cm)</label>
+            <Field
+              name="height"
+              type="text"
+              id="height"
+              placeholder="height"
+              class="p-2 rounded-lg border-none outline-none text-black ml-9 w-input"
+              rules="required|number"
+              :value="props.height"
+              @input="$emit('update:height', $event.target.value)"
+            />
+            <ErrorMessage name="height" class="text-sm block text-red-600 mt-4" />
+          </div>
+          <div>
+            <label for="width">Width (cm)</label>
+            <Field
+              name="width"
+              type="text"
+              id="width"
+              placeholder="width"
+              class="p-2 rounded-lg border-none outline-none text-black ml-10 w-input"
+              rules="required|number"
+              :value="props.width"
+              @input="$emit('update:width', $event.target.value)"
+            />
+            <ErrorMessage name="width" class="text-sm block text-red-600 mt-4" />
+          </div>
+          <div>
+            <label for="length">Length (cm)</label>
+            <Field
+              name="length"
+              type="text"
+              id="length"
+              placeholder="length"
+              class="p-2 rounded-lg border-none outline-none text-black ml-8 w-input"
+              rules="required|number"
+              :value="props.length"
+              @input="$emit('update:length', $event.target.value)"
+            />
+            <ErrorMessage name="length" class="text-sm block text-red-600 mt-4" />
+
+            <p class="text-sm mt-6 text-gray-300">*Please, provide dimensions in CM</p>
+          </div>
+        </div>
+
+        <p v-else class="text-center text-lg">Product type is not selected yet</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Field, ErrorMessage } from 'vee-validate'
+
+const props = defineProps({
+  selectedProductType: {
+    required: true
+  },
+  size: {
+    required: false
+  },
+  weight: {
+    required: false
+  },
+  height: {
+    required: false
+  },
+  width: {
+    required: false
+  },
+  length: {
+    required: false
+  }
+})
+</script>
--- public/src/views/AddProductsView.vue
+++ public/src/views/AddProductsView.vue
@@ -13,90 +13,20 @@
         @update:price="price = $event"
         @update:selectedProductType="selectedProductType = $event"
       ></input-data>
-      <div>
-        <div class="w-content bg-gray-950 h-contentH mt-12 rounded-lg drop-shadow-lg">
-          <div class="p-10">
-            <div v-if="selectedProductType === 'DVD'" class="">
-              <label for="size">Size (mb)</label>
-              <Field
-                name="size"
-                type="text"
-                id="size"
-                placeholder="size"
-                class="p-2 rounded-lg border-none outline-none text-black ml-10 w-input"
-                rules="required|number"
-                v-model="size"
-              />
-              <ErrorMessage name="size" class="text-sm block text-red-600 mt-4" />
-
-              <p class="text-sm mt-4 text-gray-300">*Please, provide disc space in MB</p>
-            </div>
-            <div v-else-if="selectedProductType === 'Book'">
-              <label for="length">Weight (kg)</label>
-              <Field
-                name="weight"
-                type="text"
-                id="weight"
-                placeholder="weight"
-                class="p-2 rounded-lg border-none outline-none text-black ml-10 w-input"
-                rules="required|number"
-                v-model="weight"
-              />
-              <ErrorMessage name="weight" class="text-sm block text-red-600 mt-4" />
-
-              <p class="text-sm mt-4 text-gray-300">*Please, provide weight in KG</p>
-            </div>
-            <div
-              v-else-if="selectedProductType === 'Furniture'"
-              class="flex flex-col space-y-8 w-content"
-            >
-              <div>
-                <label for="height">Height (cm)</label>
-                <Field
-                  name="height"
-                  type="text"
-                  id="height"
-                  placeholder="height"
-                  class="p-2 rounded-lg border-none outline-none text-black ml-9 w-input"
-                  rules="required|number"
-                  v-model="height"
-                />
-                <ErrorMessage name="height" class="text-sm block text-red-600 mt-4" />
-              </div>
-              <div>
-                <label for="width">Width (cm)</label>
-                <Field
-                  name="width"
-                  type="text"
-                  id="width"
-                  placeholder="width"
-                  class="p-2 rounded-lg border-none outline-none text-black ml-10 w-input"
-                  rules="required|number"
-                  v-model="width"
-                />
-                <ErrorMessage name="width" class="text-sm block text-red-600 mt-4" />
-              </div>
-              <div>
-                <label for="length">Length (cm)</label>
-                <Field
-                  name="length"
-                  type="text"
-                  id="length"
-                  placeholder="length"
-                  class="p-2 rounded-lg border-none outline-none text-black ml-8 w-input"
-                  rules="required|number"
-                  v-model="length"
-                />
-                <ErrorMessage name="length" class="text-sm block text-red-600 mt-4" />
-
-                <p class="text-sm mt-6 text-gray-300">*Please, provide dimensions in CM</p>
-              </div>
-            </div>
-
-            <p v-else class="text-center text-lg">Product type is not selected yet</p>
-          </div>
-        </div>
-      </div>
+      <product-types
+        :size="size"
+        :weight="weight"
+        :height="height"
+        :width="width"
+        :length="length"
+        :selectedProductType="selectedProductType"
+        @update:size="size = $event"
+        @update:weight="weight = $event"
+        @update:height="height = $event"
+        @update:width="width = $event"
+        @update:length="length = $event"
+        @update:selectedProductType="selectedProductType = $event"
+      ></product-types>
     </div>
 
     <footer class="fixed w-full bottom-12">
@@ -106,9 +36,11 @@
   </Form>
 </template>
 <script setup>
-import { Form, Field, ErrorMessage } from 'vee-validate'
+import { Form } from 'vee-validate'
 import BaseHeader from '@/components/layout/BaseHeader.vue'
 import InputData from '@/components/InputData.vue'
+import ProductTypes from '@/components/ProductTypes.vue'
+
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import axios from 'axios'
@@ -149,6 +81,7 @@ const addProduct = async () => {
       }
     })
     .then(() => {
+      console.log(formData)
       router.push({ name: 'home' })
     })
     .catch((error) => {
