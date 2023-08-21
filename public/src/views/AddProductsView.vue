7a1963986dce5b2c06dfc1a4170d63d29c9b2d97
heyosseus <ratiruxadzee@gmail.com>
2023-08-21 15:05:29 +0400

    refactor: modify existing file and change its behavior to satisfy tests requirements

--- public/src/views/AddProductsView.vue
+++ public/src/views/AddProductsView.vue
@@ -1,55 +1,147 @@
 <template>
-  <Form @submit="addProduct" class="py-10 px-6   lg:px-16" id="product_form">
-    <base-header save="Save" cancel="Cancel" :addProduct="addProduct"></base-header>
-    <div
-      v-if="alreadyExist"
-      class="px-5 py-3 bg-red-500 text-center absolute bottom-24 left-20 rounded-lg cursor-pointer text-lg"
-      @click="alreadyExist = false"
-    >
-      SKU name is already exists
-    </div>
-    <div class="w-full h-[1px] bg-gray-400 mt-4"></div>
-    <div class="flex flex-col lg:items-start lg:p-5 lg:flex-row md:justify-between" @submit.prevent>
-      <input-data
-        :sku="sku"
-        :name="name"
-        :price="price"
-        :selectedProductType="selectedProductType"
-        @update:sku="sku = $event"
-        @update:name="name = $event"
-        @update:price="price = $event"
-        @update:selectedProductType="selectedProductType = $event"
-      ></input-data>
-      <product-types
-        :size="size"
-        :weight="weight"
-        :height="height"
-        :width="width"
-        :length="length"
-        :selectedProductType="selectedProductType"
-        @update:size="size = $event"
-        @update:weight="weight = $event"
-        @update:height="height = $event"
-        @update:width="width = $event"
-        @update:length="length = $event"
-        @update:selectedProductType="selectedProductType = $event"
-      ></product-types>
-    </div>
+  <div>
+    <Form @submit="addProduct" class="py-10 px-6 lg:px-16" id="product_form">
+      <div
+        v-if="alreadyExist"
+        class="px-5 py-3 bg-red-500 text-center absolute bottom-24 left-20 rounded-lg cursor-pointer text-lg"
+        @click="alreadyExist = false"
+      >
+        SKU name is already exists
+      </div>
+      <base-header save="Save" cancel="Cancel" :addProduct="addProduct"></base-header>
+      <div class="w-full h-[1px] bg-gray-400 mt-4"></div>
 
-    <BaseFooter />
-  </Form>
+      <div class="flex flex-col w-inputs justify-between space-y-4 mt-10">
+        <div class="w-full justify-between flex items-center">
+          <label for="sku">SKU</label>
+          <Field
+            name="sku"
+            type="text"
+            id="sku"
+            v-model="sku"
+            class="p-2 rounded-lg border-none outline-none text-black ml-auto w-fit lg:w-input uppercase placeholder:lowercase"
+            placeholder="sku"
+            rules="required"
+          />
+        </div>
+        <ErrorMessage name="sku" class="text-sm text-red-600 ml-24" />
+
+        <div class="w-full justify-between flex items-center">
+          <label for="name">Name</label>
+          <Field
+            name="name"
+            type="text"
+            id="name"
+            v-model="name"
+            class="p-2 rounded-lg border-none outline-none text-black ml-auto w-fit lg:w-input placeholder:lowercase"
+            placeholder="name"
+            rules="required"
+          />
+        </div>
+        <ErrorMessage name="name" class="text-sm text-red-600 ml-24 mt-2" />
+
+        <div class="w-full justify-between flex items-center">
+          <label for="price">Price ($)</label>
+          <Field
+            name="price"
+            type="number"
+            id="price"
+            v-model="price"
+            class="p-2 rounded-lg border-none outline-none text-black ml-auto w-fit lg:w-input uppercase placeholder:lowercase"
+            placeholder="price"
+            rules="required"
+          />
+        </div>
+        <ErrorMessage name="price" class="text-sm text-red-600 ml-24 mt-2" />
+      </div>
+
+      <label for="productType">Type switcher:</label>
+      <select
+        name="selector"
+        id="productType"
+        v-model="productType"
+        class="ml-10 p-2 bg-transparent outline-0 w-[352px] mt-6"
+      >
+        <option value="DVD" id="DVD" class="bg-primary text-white">DVD</option>
+        <option value="Book" id="Book" class="bg-primary text-white">Book</option>
+        <option value="Furniture" id="Furnitue" class="bg-primary text-white">Furniture</option>
+      </select>
+      <div v-if="productType === 'DVD'" class="mt-6">
+        <label for="size">Size</label>
+        <Field
+          name="size"
+          type="text"
+          id="size"
+          v-model="size"
+          class="p-2 rounded-lg border-none outline-none text-black ml-16 w-fit lg:w-input uppercase placeholder:lowercase"
+          placeholder="size"
+          rules="required|number"
+        />
+      </div>
+      <ErrorMessage name="size" class="text-sm text-red-600 ml-24" />
+
+      <div v-if="productType === 'Book'" class="mt-6">
+        <label for="weight">Weight</label>
+        <Field
+          name="weight"
+          type="text"
+          id="weight"
+          v-model="weight"
+          class="p-2 rounded-lg border-none outline-none text-black ml-11 w-fit lg:w-input uppercase placeholder:lowercase"
+          placeholder="weight"
+          rules="required|number"
+        />
+      </div>
+      <ErrorMessage name="weight" class="text-sm text-red-600 ml-24" />
+
+      <div v-if="productType === 'Furniture'" class="flex flex-col space-y-1 mt-6">
+        <label for="height">Height</label>
+        <Field
+          name="height"
+          type="text"
+          id="height"
+          v-model="height"
+          class="p-2 rounded-lg border-none outline-none text-black w-fit lg:w-inputs uppercase placeholder:lowercase"
+          placeholder="height"
+          rules="required|number"
+        />
+        <ErrorMessage name="height" class="text-sm text-red-600" />
+        <label for="width">Width</label>
+        <Field
+          name="width"
+          type="text"
+          id="width"
+          v-model="width"
+          class="p-2 rounded-lg border-none outline-none text-black w-fit lg:w-inputs uppercase placeholder:lowercase"
+          placeholder="width"
+          rules="required|number"
+        />
+        <ErrorMessage name="width" class="text-sm text-red-600" />
+        <label for="length">Length</label>
+        <Field
+          name="length"
+          type="text"
+          id="length"
+          v-model="length"
+          class="p-2 rounded-lg border-none outline-none text-black w-fit lg:w-inputs uppercase placeholder:lowercase"
+          placeholder="length"
+          rules="required|number"
+        />
+        <ErrorMessage name="length" class="text-sm text-red-600" />
+      </div>
+    </Form>
+    <BaseFooterVue />
+  </div>
 </template>
+
 <script setup>
-import { Form } from 'vee-validate'
-import BaseHeader from '@/components/layout/BaseHeader.vue'
-import BaseFooter from '@/components/layout/BaseFooter.vue'
-import InputData from '@/components/InputData.vue'
-import ProductTypes from '@/components/ProductTypes.vue'
-// import AxiosInstance from '@/config/index.js'
-import axios from 'axios'
+import { Form, Field, ErrorMessage } from 'vee-validate'
 import { ref } from 'vue'
+import BaseHeader from '../components/layout/BaseHeader.vue'
+import BaseFooterVue from '../components/layout/BaseFooter.vue'
+import axios from 'axios'
 import { useRouter } from 'vue-router'
-const backendURL = import.meta.env.VITE_PUBLIC_API
+
 const router = useRouter()
 const sku = ref('')
 const name = ref('')
@@ -59,39 +151,39 @@ const weight = ref('')
 const height = ref('')
 const width = ref('')
 const length = ref('')
-const selectedProductType = ref('')
-const alreadyExist = ref(false)
+const productType = ref('')
 
-const addProduct = async () => {
+const backendURL = import.meta.env.VITE_PUBLIC_API
+
+const addProduct = () => {
   const formData = {
     action: 'addProducts',
     sku: sku.value,
     name: name.value,
     price: parseFloat(price.value),
-    type: selectedProductType.value
+    type: productType.value
   }
-  if (selectedProductType.value === 'DVD') {
+  if (productType.value === 'DVD') {
     formData.size = parseFloat(size.value)
-  } else if (selectedProductType.value === 'Book') {
+  } else if (productType.value === 'Book') {
     formData.weight = parseFloat(weight.value)
-  } else if (selectedProductType.value === 'Furniture') {
+  } else if (productType.value === 'Furniture') {
     formData.height = parseFloat(height.value)
     formData.width = parseFloat(width.value)
     formData.length = parseFloat(length.value)
   }
 
-  await axios
+  axios
     .post(`${backendURL}/requests/action.php`, formData, {
       withCredentials: false
     })
     .then(() => {
-      console.log(formData)
       router.push({ name: 'home' })
     })
     .catch((error) => {
-      if (error.response && error.response.status === 500) {
-        alreadyExist.value = true
-      }
+      console.error(error)
     })
 }
 </script>
+
+<style></style>
